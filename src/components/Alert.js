b87828ca85ea0ce224998ec3b2df3dae32bb5801
Artem <rozen19942810@gmail.com>
2021-01-05 21:58:15 +0200

    upd

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,5 +1,6 @@
 import React, {useContext} from "react"
 import {AlertContext} from "../context/alert/alertContext";
+import {CSSTransition} from 'react-transition-group'
 
 export const Alert = () => {
 
@@ -8,16 +9,27 @@ export const Alert = () => {
     if (!alert.visible) return null
 
     return (
-        <div className={`alert alert-${alert.type || 'warning'} alert-dismissible alertBlock`}>
-            {alert.text}
-            <button
-                onClick={hide}
-                type="button"
-                className="btn btn-outline-dark btn-sm"
-                aria-label="Close"
-            >
-                &times;
-            </button>
-        </div>
+        <CSSTransition
+            in={alert.visible}
+            timeout={{
+                enter: 500,
+                exit: 350
+            }}
+            classNames={'alert'}
+            // mountOnEnter
+            unmountOnExit
+        >
+            <div className={`alert alert-${alert.type || 'warning'} alert-dismissible alertBlock`}>
+                {alert.text}
+                <button
+                    onClick={hide}
+                    type="button"
+                    className="btn btn-outline-dark btn-sm"
+                    aria-label="Close"
+                >
+                    &times;
+                </button>
+            </div>
+        </CSSTransition>
     )
 }
